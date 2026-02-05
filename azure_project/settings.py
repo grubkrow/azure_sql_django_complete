e13f88eaa1c0ad9a1d3d0d719f069d7d636b8beb
grubkrow <kongpob921@gmail.com>
2026-02-05 15:25:12 +0700

    Update settings.py

--- azure_project/settings.py
+++ azure_project/settings.py
@@ -90,10 +90,10 @@ WSGI_APPLICATION = 'azure_project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'mssql',
-        'NAME': 'bearlab-sql-db',
-        'USER': 'bearlab-admin',
-        'PASSWORD': 'P@ss1234',
-        'HOST': 'bearlab-sql-server.database.windows.net',
+        'NAME': 'free-sql-db-1726578',
+        'USER': 'grubkrow',
+        'PASSWORD': 'EWR2019asd',
+        'HOST': 'grubkrow.database.windows.net',
         'PORT': '1433',
         'OPTIONS': {
             'driver': 'ODBC Driver 18 for SQL Server',
