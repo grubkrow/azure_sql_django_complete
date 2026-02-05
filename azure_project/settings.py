a6d3102c64f2430e82e42d37b00e8a15f00d5071
grubkrow <kongpob921@gmail.com>
2026-02-05 16:57:07 +0700

    Update settings.py

--- azure_project/settings.py
+++ azure_project/settings.py
@@ -91,7 +91,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'mssql',
         'NAME': 'free-sql-db-1726578',
-        'USER': 'grubkrow',
+        'USER': 'kongphob',
         'PASSWORD': 'EWR2019asd',
         'HOST': 'grubkrow.database.windows.net',
         'PORT': '1433',
@@ -108,7 +108,7 @@ import urllib.parse
 mongo_username = urllib.parse.quote_plus('kongphob')
 mongo_password = urllib.parse.quote_plus('EWR2019asd')
 # MONGO_URI = 'mongodb://localhost:27017/'
-MONGO_URI = f'mongodb+srv://kongphob:{mongo_password}@kongphob-mongodb.global.mongocluster.cosmos.azure.com/?tls=true&authMechanism=SCRAM-SHA-256&retrywrites=false&maxIdleTimeMS=120000'
+MONGO_URI = f'mongodb+srv://{mongo_username}:{mongo_password}@kongphob-mongodb.global.mongocluster.cosmos.azure.com/?tls=true&authMechanism=SCRAM-SHA-256&retrywrites=false&maxIdleTimeMS=120000'
 MONGO_DB_NAME = 'django_store_reviews'
 
 # Password validation
